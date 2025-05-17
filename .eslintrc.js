52b8f5eeb7a8a008e1ef0e22405a3818974f7c4c
Owen <owen@example.com>
2025-05-17 08:16:57 +0800

    fix: ensure eslint plugins and parser loaded for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: 'next',
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended' // 👈 加這行才會生效
+  ],
+  plugins: ['@typescript-eslint'], // 👈 告訴 ESLint 要用這個 plugin
+  parser: '@typescript-eslint/parser', // 👈 使用 TypeScript 專用解析器
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
-    '@next/next/no-img-element': 'off',
-  },
+    '@next/next/no-img-element': 'off'
+  }
 };
