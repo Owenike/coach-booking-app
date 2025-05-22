94cfeb772a94d3901001f74330c75d18e6c65a32
Owen <owen@example.com>
2025-05-22 22:30:51 +0800

    fix: 禁用 next export 模式，避免 /enter-password build 錯誤

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // ✅ 告訴 Next.js 不要進行 export（避免 Static Export）
+  output: 'standalone',
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
